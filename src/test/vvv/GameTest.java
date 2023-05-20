da5769283dd2327abac971e96261108510db6bf2
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-05-20 11:43:12 +0200

    Remove unused import

--- src/test/vvv/GameTest.java
+++ src/test/vvv/GameTest.java
@@ -1,7 +1,6 @@
 package vvv;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
